20b33055220b0e4f21d978abe82fabed0bbb715f
joln17 <33175263+joln17@users.noreply.github.com>
2019-10-19 13:51:25 +0200

    Save hist data

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const coinCapWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin');
+const histData = [];
+let savedTimestamp = 0;
+
+// Save historical data for 1 day, logged every minute
+coinCapWs.on('message', message => {
+    message = JSON.parse(message);
+    message.timestamp = Date.now();
+    if (message.timestamp - savedTimestamp >= 60000) {
+        histData.push(message);
+        if (histData.length > 1440) {
+            histData.shift();
+        }
+        savedTimestamp = message.timestamp;
+        console.log(JSON.stringify(message));
+    }
+});
 
 wss.on('connection', ws => {
+    ws.send( JSON.stringify({ histData: histData }));
     coinCapWs.on('message', message => {
-        console.log(message);
-        ws.send(message);
+        message = JSON.parse(message);
+        message.timestamp = Date.now();
+        ws.send(JSON.stringify({ rtData: message }));
     });
 });
 
